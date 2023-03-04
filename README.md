0b54241df4d3ae9b2429e8bcdc7c1dff60733fe8
Gonzalo Plaza <gonzalo.plaza@retargetly.com>
2023-03-04 05:46:16 -0300

    completed app

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # counterapp
 Simple web app used to learn aws services
+
+## Road map
+
+1. How to create and host a web page? **Using AWS Amplify**
+
+2. How to perform math operations? **With Lambdas**
+
+3. How to invoke the math functionality? **With API Gateway**
+
+4. How to store the results? **With DynamoDB**
+
+5. How to handle permissions? **With IAM**
+
+## Reference
+
+https://www.youtube.com/watch?v=7m_q1ldzw0U
+
+## Architecture
+
+![alt text](https://github.com/gonzaplazag/counterapp/blob/main/pii-enrichment-migration-counterapp.png?raw=true)
---
+++ lambda/counterAppLambda/lambda.py
@@ -0,0 +1,24 @@
+import json
+import math
+import boto3
+from time import gmtime, strftime
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table("counterappDB")
+
+now = strftime("%a, %d %b %Y %H:%M:%S + 0000", gmtime())
+
+def lambda_handler(event, context):
+    mathResult = math.pow(int(event['base']), int(event['exp']))
+
+    response = table.put_item(
+        Item={
+            "ID": str(mathResult),
+            "LatestGreetingTime": now
+        }
+    )
+
+    return {
+        'statusCode' : 200,
+        'body' : json.dumps('Your result is ' + str(mathResult))
+    }
